80c299eb651d00c27cb7b9aafc170681f65f4b4b
Zainul-aabidheen <128073694+Zainul-aabidheen@users.noreply.github.com>
2024-01-24 16:51:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+// Password check
 const enteredPassword = prompt("Enter the password to access the portal:");
 
 if (enteredPassword === "X4iyP") {
+    // Show the table if the password is correct
     document.getElementById("votersTable").classList.remove("hidden");
 
-    loadVotingStatusRealTime();
+    // Load voting status from Firebase
+    loadVotingStatus();
 } else {
+    // Display an alert and hide the table if the password is incorrect
     alert("Incorrect password. You do not have permission to access the portal.");
     document.getElementById("votersTable").classList.add("hidden");
 }
